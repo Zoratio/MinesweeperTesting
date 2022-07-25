7ea75090854ea3dc26f3d7588aa2f6dd747eb90e
Taylor Bromley <taylorbromley@sky.com>
2022-07-25 14:27:50 +0100

    Added basic test

---
+++ src/test/java/com/TSI/minesweeper/testing/MapTest.java
@@ -0,0 +1,12 @@
+package com.TSI.minesweeper.testing;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class MapTest {
+    @Test
+    public void test_map(){
+        int integer = 5;
+        assertEquals(5, integer);
+    }
+}
